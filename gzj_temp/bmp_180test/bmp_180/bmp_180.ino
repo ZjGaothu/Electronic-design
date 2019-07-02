1c7dfa8acbaffa907e9d11007a8f3e5aed149613
THU_gzj <48663858+ZjGaothu@users.noreply.github.com>
2019-07-02 14:32:03 +0800

    气压检测

---
+++ gzj_temp/bmp_180test/bmp_180/bmp_180.ino
@@ -0,0 +1,41 @@
+#include"SFE_BMP180.h"
+
+SFE_BMP180 AirPresure;
+char presureDelayTime;
+double presureP, presureT;
+
+void setup() {
+  Serial.begin(9600);
+  AirPresure.begin();
+}
+
+void loop()
+{
+  presureDelayTime = AirPresure.startPressure(3);
+  if (presureDelayTime != 0)
+  {
+    delay(presureDelayTime);
+    presureDelayTime = AirPresure.getPressure(presureP, presureT);
+    if (presureDelayTime != 0)
+    {
+      //当前气压
+      Serial.print("当前气压是: ");
+      Serial.print(presureP);
+      Serial.println(" bar");
+
+      //换算成标准大气压
+      Serial.print("换算标准大气压：");
+      Serial.print(presureP / 1000.0);
+      Serial.println(" atm");
+    }
+    else
+    {
+      Serial.println("ERROR");
+    }
+  }
+  else
+  {
+    Serial.println("ERROR");
+  }
+  delay(1000);
+}
