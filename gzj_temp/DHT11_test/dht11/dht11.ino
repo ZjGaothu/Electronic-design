509132bf96c077886b5c29d5da729eb2b5bbe29b
MapleHock <wu.wenxu@outlook.com>
2019-07-05 23:41:35 +0800

    7.5 afternoon log

--- gzj_temp/DHT11_test/dht11/dht11.ino
+++ gzj_temp/DHT11_test/dht11/dht11.ino
@@ -15,17 +15,12 @@ uint8_t heart[8] = {B00000,B11110,B11010,B01001,B11110,B11010,B01001};
 
 void setup(){
   Serial.begin(9600);
-  Serial.println();
-  lcd.createChar(0,heart);
-  while(Serial.read() >= 0) {} // clear serial port when begin
-  lcd.init();                  // 初始化LCD
-  lcd.backlight();             //设置LCD背景等亮
-  lcd.home();
+  Serial.println("serial");
 }
 
 
 void loop() {
-  Serial.println("\n");
+  Serial.println("lp");
 
   int chk = DHT11.read(DHT11PIN);
 
@@ -51,18 +46,9 @@ void loop() {
   Serial.print("Humidity (%): ");
   Serial.println((float)DHT11.humidity, 2);
   float hum = (float)DHT11.humidity;
-  lcd.setCursor(0, 0);
-  lcd.print("Hum : ");
-  lcd.print(hum);
-  lcd.print(" %");
   Serial.print("Temperature °C): ");
   Serial.println((float)DHT11.temperature, 2);
   float tem = (float)DHT11.temperature;
-  lcd.setCursor(0, 1);
-  lcd.print("Temp: ");
-  lcd.print(tem);
-  lcd.print((char)223);
-  lcd.print("C ");
   
 
 
