d9243dc2cdb1c71cb06a21c232d2fa90b6314c4d
MapleHock <wu.wenxu@outlook.com>
2019-07-07 14:11:25 +0800

    1.1 joint release, add ir bluetooth sd write

--- project/EnvMeter/EnvMeter.ino
+++ project/EnvMeter/EnvMeter.ino
@@ -1,4 +1,15 @@
-//TODO change dht11 library
+// pin configuration
+/*
+dht11 - 8 temperature and humidity sensor
+GP2Y1050AU0F signal - A0(remember filter); led - 12
+light_pulse - 13
+IR - 9
+servo - 5
+chaos - A1
+SD MOSI - 11; MISO - 12; SCK - 13; CS - 4
+I2C device
+A4 - SDA, A5 - SCK
+*/
 #include <Wire.h> //I2C device library
 #include "SFE_BMP180.h"
 #include "dht11.h"
@@ -16,6 +27,7 @@
 #define IR_PIN 9 // IR remote receiver pin
 #define SERVO_PIN 5 // TODO check, PWM need
 #define CHAOS_PIN A1
+#define SD_CS_PIN 4
 
 // I2C use hard I2C in arduino A4 - SDA, A5 - SCK
 // I2C device, define address
@@ -39,9 +51,42 @@ const unsigned int show_refresh_time PROGMEM = 700; // ms
 // measure storeage
 float dustVal = 0;
 double presureP,presureT; // TODO maybe cancel 1 variable
-uint8_t key_radar_code; //temp P7-P4 for key, P3 for radar TODO check it
 int light_pulse_time = 0;
 decode_results ir_code;
+
+uint8_t key_radar_code; //temp P7-P4 for key, P3 for radar TODO check it
+// key setting
+/*
+P7 - P4
+up down start/stop_roll set_sensitivity
+*/
+
+// IR remote controller code
+/*
+    CH-     CH      CH+
+    FFA25D  FF629D  FFE21D
+    |<<     >>|     >||
+    FF22DD  FF02FD  FFC23D
+    -       +       EQ
+    FFE01F  FFA857  FF906F
+    0       100+    200+
+    FF6897  FF9867  FFB04F
+    1       2       3
+    FF30CF  FF18E7  FF7A85
+    4       5       6
+    FF10EF  FF38C7  FF5AA5
+    7       8       9
+    FF42BD  FF4AB5  FF52AD
+*/
+#define UP_IR 0xffa857
+#define DOWN_IR 0xffe01f
+#define ROLL_IR 0xff906f
+#define K0_IR 0xff6897
+#define K1_IR 0xff30cf
+#define K2_IR 0xff9867
+#define K3_IR 0xff7a85
+#define K4_IR 0xff10ef
+#define K5_IR 0xff38c7
 
 // Byte set as follow, write 
 /*
@@ -68,6 +113,9 @@ void Btn2State();
 void PrintLCDByState();
 void DecodeAndPrint(uint8_t print_code);
 void IR2State();
+uint8_t IRNum2Num(const decode_results& ir_num);
+void BluetoothSend();
+void FileWrite();
 
 void setup() {
     Serial.begin(9600); // serial to bluetooth by module HC-06 TODO debug use, maybe end
@@ -75,6 +123,9 @@ void setup() {
     air_presure_sensor.begin();
     irrecv.enableIRIn();
     radar_sweeper.attach(SERVO_PIN);
+    if (!SD.begin(SD_CS_PIN)) {
+        Serial.print(F("no SD card"));
+    }
     lcd.init();
     lcd.backlight();
     pinMode(DUST_LED_PIN, OUTPUT);
@@ -124,14 +175,19 @@ void loop() {
     IR2State();
     // show
     PrintLCDByState();
+
+    // bluetooth send
+    BluetoothSend();
+    // write in SD card
+    FileWrite();
 }
 
 // use servo to Sweep Radar, check if there is animal
 // TODO inline or marco?
 void SweepRadar() {
-    radar_sweeper.write(analogRead(CHAOS_PIN) * 180 / 1023); // turn it to 0-180 TODO change formula?
+    radar_sweeper.write(analogRead(CHAOS_PIN) % 181); // turn it to 0-180 TODO change formula?
     delayMicroseconds(100); // TODO check time
-    radar_sweeper.write(analogRead(CHAOS_PIN) * 180 / 1023); // turn it to 0-180 TODO change formula?
+    radar_sweeper.write(analogRead(CHAOS_PIN) % 181); // turn it to 0-180 TODO change formula?
 }
 
 void Btn2State() {
@@ -255,6 +311,107 @@ void DecodeAndPrint(uint8_t code) {
 }
 
 void IR2State() {
+    if (!irrecv.decode(&ir_code)) { // get nothing from IR
+        return;
+    }
+    if (ir_code.value == ROLL_IR) {
+        state ^= 0x01; // reverse P0
+        irrecv.resume();
+        return;
+    }    
+    if (state & 0x01 != 0) { // only in stop roll mode need to deal other key
+        switch (ir_code.value)
+        {
+        case UP_IR:
+            if (state & 0x70 == 0) { // the first line show temperature(code 0) now, set for circle to animal detect(code 5)
+                state += 0x60;
+            }
+            if (state & 0x0e == 0) { // the second line show temperature(code 0) now, set for circle to animal detect(code 5)
+                state += 0x0c;
+            }
+            state -= 0x12; // up line, both 3 bits show code -1
+            break;
+        case DOWN_IR:
+            state += 0x12;
+            if (state & 0x70 == 0x60) { // the first line show animal detect(code 5) before, set now for circle to temperature(code 0)
+                state &= 0x0f;
+            }
+            if (state & 0x0e == 0x0c) { // the second line show animal detect(code 5) before, set now for circle to animal detect
+                state &= 0xf1;
+            }
+            // up line, both 3 bits show code +1
+            break;
+        default: // self define order TODO hide other useless key
+            state = state & 0x8f + (IRNum2Num(ir_code) << 4); // set the first line
+            irrecv.resume();
+            delay(10); //TODO whether delay
+            if (!irrecv.decode(&ir_code)) {
+                break;
+            }
+            state = state & 0x8f + (IRNum2Num(ir_code) << 1); // set the second line
+            break;
+        }
+        irrecv.resume();
+        return;
+    }
+    return;
+}
 
+uint8_t IRNum2Num(const decode_results& ir_num) {
+    switch (ir_num.value) {
+    case K0_IR:
+        return 0;
+    case K1_IR:
+        return 1;
+    case K2_IR:
+        return 2;
+    case K3_IR:
+        return 3;
+    case K4_IR:
+        return 4;
+    case K5_IR:
+        return 5;
+    default:
+        break;
+    }
+    return 0xff; // for error TODO cancel it
+}
+
+void BluetoothSend() {
+    Serial.print(t_h_sensor.temperature_int);Serial.print(F("."));Serial.print(t_h_sensor.temperature_dec);
+    Serial.print(F(","));
+    Serial.print(t_h_sensor.humidity_int);Serial.print(F("."));Serial.print(t_h_sensor.humidity_dec);
+    Serial.print(F(","));
+    Serial.print(presureP);
+    Serial.print(F(","));
+    Serial.print(dustVal);
+    Serial.print(F(","));
+    Serial.print(light_pulse_time);
+    Serial.print(F(","));
+    Serial.print(key_radar_code & 0x08);
+    Serial.println(F(","));
+}
+
+void FileWrite() {
+    record_file = SD.open(F("record.csv")); //TODO multi file name
+    if (!record_file) {
+        Serial.print(F("no space"));
+        return;
+    } 
+    record_file.print(millis());
+    record_file.print(F(","));
+    record_file.print(t_h_sensor.temperature_int);record_file.print(F("."));record_file.print(t_h_sensor.temperature_dec);
+    record_file.print(F(","));
+    record_file.print(t_h_sensor.humidity_int);record_file.print(F("."));record_file.print(t_h_sensor.humidity_dec);
+    record_file.print(F(","));
+    record_file.print(presureP);
+    record_file.print(F(","));
+    record_file.print(dustVal);
+    record_file.print(F(","));
+    record_file.print(light_pulse_time);
+    record_file.print(F(","));
+    record_file.print(key_radar_code & 0x08);
+    record_file.println(F(","));
+    record_file.close();
     return;
 }
